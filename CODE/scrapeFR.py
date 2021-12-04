a4e983c16826a585f98ec7e50f467349ec79bc2b
fwoofyFloofy <BISCUITISNOTACOOKIE@protonmail.com>
2021-12-04 15:20:03 +0000

    added readme.md

--- CODE/scrapeFR.py
+++
@@ -1,8 +0,0 @@
-from twitter_scraper_selenium import scrap_profile
-
-microsoft = scrap_profile(twitter_username="fwuffyboi",output_format="json",browser="firefox")
-
-ms = open("ms.txt", "w+")
-ms.write(microsoft)
-ms.close()
-print(microsoft['fwuffyboi'][0]['content'])
