9a5421b8a223b7cde5d969d1de932a932de74977
fwoofyFloofy <BISCUITISNOTACOOKIE@protonmail.com>
2021-12-04 15:15:14 +0000

    added all needed files (yayyyyyy)

--- CODE/ts.py
+++ CODE/ts.py
@@ -2,10 +2,11 @@ from twitter_scraper_selenium import scrap_profile
 import json
 import os
 
-usernameFile = open("usernames.txt", "r")
+usernameFile = open("../ASSETS/usernames.txt", "r")
 usernameCount = 0
 
-os.chdir("C:\\Users\\User1\\Desktop\\PROJECT_FRNN\\content_got")
+os.chdir("/content_got")
+printed = ""
 
 for username in usernameFile:
 
@@ -13,14 +14,18 @@ for username in usernameFile:
     usernameCount += 1
 
     currentUsernameLink = f"https://twitter.com/@{currentUsername}"
-    print(f"{str(usernameCount)}.{currentUsername}{currentUsernameLink}")
+    print(f"{str(usernameCount)}.{currentUsername}.{currentUsernameLink}")
 
-    scrapedInfo = scrap_profile(twitter_username=str(currentUsername), output_format="json", browser="firefox")
+    scrapedInfo = scrap_profile(twitter_username=str(currentUsername), output_format="json", browser="firefox",
+                                tweets_count=1000)
 
-    scrapedInfo = json.loads(scrapedInfo)
+    scrapedInfo = json.loads(str(scrapedInfo))
 
     for k, v in scrapedInfo.items():
         print(v["content"])
+        # printed += f"{username} Completed."
+        # printed += v['content'] + "\n"
+
         # contentFileName = currentUsername + "-CONTENT.txt"
         # contentFile = open(contentFileName, "w+")
         # contentFile.writelines(v["content"])
